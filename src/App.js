182347395511a22db2eaf767871de47919cad429
kk <z15308028010@aliyun.com>
2021-05-21 17:09:35 +0800

    react nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import './App.css'
+import React, {Fragment } from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>  
-          Learn React
-      </header>
-    </div>
+    <Fragment>
+      <h3>团队组织</h3>
+      <ul className="App">
+        <li>腾讯 AlloyTeam 团队</li>
+        <li>ISUX</li>
+        <li>FEX</li>
+        <li>淘宝前端团队（FED）</li>
+        <li>凹凸实验室</li>
+        <li>奇舞团</li>
+        <li> 阿里巴巴国际UED团队</li>
+        <li>EFE</li>
+  </ul>
+  </Fragment>
   );
 }
 
