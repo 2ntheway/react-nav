6e7d52ffcf8809d471eb05f0202718ebf7770b44
kk <z15308028010@aliyun.com>
2021-05-24 23:46:25 +0800

    创建表单按钮点击事件绑定函数用于更新state中的数据

--- src/Content.js
+++ src/Content.js
@@ -19,6 +19,7 @@ class Content extends Component {
     this.handleInputTitle = this.handleInputTitle.bind(this);
     this.handleInputDesc = this.handleInputDesc.bind(this);
     this.handleInputGroup = this.handleInputGroup.bind(this);
+    this.handleBthClick = this.handleBthClick.bind(this);
   }
   componentDidMount() {
     axios
@@ -63,7 +64,7 @@ class Content extends Component {
               value={this.state.inputValue.desc}
               onChange={this.handleInputDesc}
             />
-            <input type="button" value="提交" />
+            <button onClick={this.handleBthClick}>提交</button>
           </div>
           <div className="bd-contex">
             {this.state.data.map(function (element, index) {
@@ -73,6 +74,7 @@ class Content extends Component {
                   <ul className="App">
                     {element.list.map((items, id) => {
                       return (
+                        // 给组件传值
                         <Item
                           key={id}
                           img={items.img}
@@ -90,24 +92,50 @@ class Content extends Component {
       </Fragment>
     );
   }
+  // 获取添加的图片信息
   handleInputImg(e) {
     this.setState({
       inputValue: { ...this.state.inputValue, ...{ img: e.target.value } },
     });
   }
+  // 获取添加的title信息
   handleInputTitle(e) {
     this.setState({
       inputValue: { ...this.state.inputValue, ...{ title: e.target.value } },
     });
   }
+  // 获取添加的描述信息
   handleInputDesc(e) {
     this.setState({
+      // 更新需要修改的属性
       inputValue: { ...this.state.inputValue, ...{ desc: e.target.value } },
     });
   }
+  // 获取添加的分组信息
   handleInputGroup(e) {
     this.setState({
       inputValue: { ...this.state.inputValue, ...{ group: e.target.value } },
+    });
+  }
+  // 提交添加的数据
+  handleBthClick() {
+    this.state.data.map((item, index) => {
+      if (item.group == this.state.inputValue.group) {
+        // 构建对象
+        const tmp = {
+          title: this.state.inputValue.title,
+          desc: this.state.inputValue.desc,
+          img: this.state.inputValue.img,
+        };
+        // 拷贝state里面的数据
+        const items = [...this.state.data];
+        // 拷贝需要修改数据的部分并添加数据
+        const item = [...items[index].list, tmp];
+        // 把修改后的数据放回去
+        items[index].list = item;
+        // 更新state里面的数据
+        this.setState({ items });
+      }
     });
   }
 }
