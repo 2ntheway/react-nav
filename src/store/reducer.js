bb293fa775e8ecdfdb70e92bfdb0808cb735eecb
kk <z15308028010@aliyun.com>
2021-06-11 18:19:07 +0800

    初始化传值问题

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -15,58 +15,37 @@ const defaultState = {
 };
 export default (state = defaultState, action) => {
   // 初始化sate数据
+  const newState = JSON.parse(JSON.stringify(state));
   switch (action.type) {
     case "init_data":
-      const newState = JSON.parse(JSON.stringify(state));
       newState.data = action.value;
       return newState;
-  }
-  // 显示控件
-  switch (action.type) {
+    // 显示控件
     case "show":
-      const newState = JSON.parse(JSON.stringify(state));
       newState.visible = action.value;
       return newState;
-  }
-  // 关闭控件;
-  switch (action.type) {
+    // 关闭控件;
     case "close":
-      const newState = JSON.parse(JSON.stringify(state));
       newState.visible = action.value;
       return newState;
-  }
-  // 添加图片信息;
-  switch (action.type) {
+    // 添加图片信息;
     case "input_img":
-      const newState = JSON.parse(JSON.stringify(state));
       newState.inputValue.img = action.value;
       return newState;
-  }
-  // 添加title信息;
-  switch (action.type) {
+    // 添加title信息;
     case "input_title":
-      const newState = JSON.parse(JSON.stringify(state));
       newState.inputValue.title = action.value;
       return newState;
-  }
-  // 添加描述信息;
-  switch (action.type) {
+    // 添加描述信息;
     case "input_desc":
-      const newState = JSON.parse(JSON.stringify(state));
       newState.inputValue.desc = action.value;
       return newState;
-  }
-  // 添加分组信息;
-  switch (action.type) {
+    // 添加分组信息;
     case "input_group":
-      const newState = JSON.parse(JSON.stringify(state));
       newState.inputValue.group = action.value;
       return newState;
-  }
-  // 提交添加的数据;
-  switch (action.type) {
+    // 提交添加的数据;
     case "submit_data":
-      const newState = JSON.parse(JSON.stringify(state));
       newState.group_errorMessage = "";
       newState.title_errorMessage = "";
       newState.desc_errorMessage = "";
@@ -83,43 +62,11 @@ export default (state = defaultState, action) => {
       if (newState.inputValue.group === "") {
         newState.group_errorMessage = "分组不能为空";
       }
+      return newState;
   }
   return defaultState;
 };
 
-// handleBthClick() {
-//   // 提示用户输入，这个地方设置为生效？待解决
-//   this.setState({
-//     group_errorMessage: "",
-//     title_errorMessage: "",
-//     desc_errorMessage: "",
-//     img_errorMessage: "",
-//   });
-
-//   if (this.state.inputValue.img === "") {
-//     this.setState({
-//       // errorMessage: "输入不能为空",
-//       img_errorMessage: "图片必选项",
-//     });
-//   }
-//   if (this.state.inputValue.title === "") {
-//     this.setState({
-//       // errorMessage: "输入不能为空",
-//       title_errorMessage: "标题必选项",
-//     });
-//   }
-//   if (this.state.inputValue.desc === "") {
-//     this.setState({
-//       // errorMessage: "输入不能为空",
-//       desc_errorMessage: "描述必选项",
-//     });
-//   }
-//   if (this.state.inputValue.group === "") {
-//     this.setState({
-//       // errorMessage: "输入不能为空",
-//       group_errorMessage: "分组必选项",
-//     });
-//   }
 //   // 数据输入完整后修改state的data数据
 //   if (
 //     this.state.inputValue.group &&
